95a56034c4713f70f053cd6936811d3d7be91908
Yaron Efrat <yaronefrat92@gmail.com>
2025-06-29 23:14:12 +0200

    [8]: Add MarkUserStatusHandler and StartEmergencySituationHandler classes for handling user status updates and starting emergency situations
    This commit introduces two new command handler classes: MarkUserStatusHandler and StartEmergencySituationHandler. The MarkUserStatusHandler publishes a UserStatusMarked event when a user's status is updated, while the StartEmergencySituationHandler publishes an EmergencySituationStarted event when a new emergency situation is initiated. Both handlers utilize an event aggregator for event publishing, enhancing the event-driven architecture of the application.

---
+++ OtChaim.Application/EmergencyEvents/Handlers/MarkUserStatusHandler.cs
@@ -0,0 +1,28 @@
+using OtChaim.Application.Common;
+using OtChaim.Application.EmergencyEvents.Commands;
+using OtChaim.Domain.EmergencyEvents.Events;
+using Yaref92.Events.Abstractions;
+
+namespace OtChaim.Application.EmergencyEvents.Handlers;
+
+public class MarkUserStatusHandler : ICommandHandler<MarkUserStatus>
+{
+    private readonly IEventAggregator _eventAggregator;
+
+    public MarkUserStatusHandler(IEventAggregator eventAggregator)
+    {
+        _eventAggregator = eventAggregator;
+    }
+
+    public async Task Handle(MarkUserStatus command, CancellationToken cancellationToken = default)
+    {
+        var userStatusMarkedEvent = new UserStatusMarked(
+            command.UserId,
+            command.EmergencySituationId,
+            command.Status,
+            command.Message);
+
+        // Publish the event through the event aggregator
+        await _eventAggregator.PublishEventAsync(userStatusMarkedEvent, cancellationToken);
+    }
+} 
