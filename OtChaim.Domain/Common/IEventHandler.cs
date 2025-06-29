bc3e35da59d472212780614c11bf8363ef076864
Yaron Efrat <yaronefrat92@gmail.com>
2025-06-29 22:39:52 +0200

    [8]: Remove IEvent and IEventHandler interfaces as they are no longer needed after transitioning to IDomainEvent. This cleanup enhances code clarity and reduces redundancy.

--- OtChaim.Domain/Common/IEventHandler.cs
+++
@@ -1,6 +0,0 @@
-namespace OtChaim.Domain.Common;
-
-public interface IEventHandler<in TEvent> where TEvent : IEvent
-{
-    Task Handle(TEvent domainEvent, CancellationToken cancellationToken = default);
-}
