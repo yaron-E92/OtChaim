87a5fc2b21b702887142c1954a30085471027549
Yaron Efrat <yaronefrat92@gmail.com>
2025-07-09 15:27:24 +0200

    [16]: Init Maui project

---
+++ OtChaim.Presentation.MAUI/App.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace OtChaim.Presentation.MAUI;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new AppShell();
+	}
+}
---
+++ OtChaim.Presentation.MAUI/MainPage.xaml.cs
@@ -0,0 +1,11 @@
+namespace OtChaim.Presentation.MAUI;
+
+public partial class MainPage : ContentPage
+{
+
+	public MainPage()
+	{
+		InitializeComponent();
+	}
+}
+
