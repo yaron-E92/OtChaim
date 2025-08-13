80e05db8154f6b974021ebd4e789315fab005c9e
Yaron Efrat <yaronefrat92@gmail.com>
2025-08-13 12:50:08 +0200

    #33 test: emergency service tests

---
+++ OtChaim.Application.Tests/Services/EmergencyDataServiceTests.cs
@@ -0,0 +1,93 @@
+using System.Collections.ObjectModel;
+using FluentAssertions;
+using NSubstitute;
+using OtChaim.Domain.EmergencyEvents;
+using OtChaim.Domain.Users;
+using OtChaim.Application.Services;
+
+namespace OtChaim.Application.Tests.Services;
+
+[TestFixture]
+public class EmergencyDataServiceTests
+{
+    private EmergencyDataService _service = null!;
+
+    [SetUp]
+    public void Setup()
+    {
+        _service = new EmergencyDataService(Substitute.For<IEmergencyRepository>(), Substitute.For<IUserRepository>());
+    }
+
+    [Test]
+    public void Constructor_ShouldInitializeService()
+    {
+        // Assert
+        _service.Should().NotBeNull();
+    }
+
+    [Test]
+    public void LoadActiveEmergenciesAsync_ShouldReturnEmptyCollection()
+    {
+        // Arrange
+        var emergencies = new ObservableCollection<Emergency>();
+
+        // Act
+        var result = _service.LoadActiveEmergenciesAsync(emergencies);
+
+        // Assert
+        result.Should().NotBeNull();
+        emergencies.Should().BeEmpty();
+    }
+
+    [Test]
+    public void LoadUsersAsync_ShouldReturnEmptyCollection()
+    {
+        // Arrange
+        var users = new ObservableCollection<User>();
+
+        // Act
+        var result = _service.LoadUsersAsync(users);
+
+        // Assert
+        result.Should().NotBeNull();
+        users.Should().BeEmpty();
+    }
+
+    [Test]
+    public  async Task LoadActiveEmergenciesAsync_ShouldCompleteSuccessfully()
+    {
+        // Arrange
+        var emergencies = new ObservableCollection<Emergency>();
+
+        // Act & Assert
+        var action = () => _service.LoadActiveEmergenciesAsync(emergencies);
+        await action.Should().NotThrowAsync();
+    }
+
+    [Test]
+    public  async Task LoadUsersAsync_ShouldCompleteSuccessfully()
+    {
+        // Arrange
+        var users = new ObservableCollection<User>();
+
+        // Act & Assert
+        var action = () => _service.LoadUsersAsync(users);
+        await action.Should().NotThrowAsync();
+    }
+
+    [Test]
+    public  async Task LoadActiveEmergenciesAsync_WithNullCollection_ShouldHandleGracefully()
+    {
+        // Act & Assert
+        var action = () => _service.LoadActiveEmergenciesAsync(null!);
+        await action.Should().NotThrowAsync();
+    }
+
+    [Test]
+    public async Task LoadUsersAsync_WithNullCollection_ShouldHandleGracefully()
+    {
+        // Act & Assert
+        var action = () => _service.LoadUsersAsync(null!);
+        await action.Should().NotThrowAsync();
+    }
+}
